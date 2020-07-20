d5a8b654fcaa78e608beeb154ab1c45e1168b2df
paulroussel <36078964+paulroussel@users.noreply.github.com>
2020-07-20 13:22:40 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/39f3lejk5dxc3nc/model_learner.pkl?dl=0'
+export_file_name = 'model_learner.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['drawing', 'painting', 'photography','work_on_paper']
 path = Path(__file__).parent
 
 app = Starlette()
